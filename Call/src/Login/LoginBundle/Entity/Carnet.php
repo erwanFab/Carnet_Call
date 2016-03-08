e97523b60f2404d22d0a5adf26e455fed43a3758
erwanFab <erwan.lhenoret@gmail.com>
2016-03-08 19:37:24 +0100

    Test carnet

--- Call/src/Login/LoginBundle/Entity/Carnet.php
+++ Call/src/Login/LoginBundle/Entity/Carnet.php
@@ -1,1 +1,41 @@
 <?php
+namespace Login\LoginBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Table(name="avatar")
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
+ */
+class Avatar 
+{
+
+	/**
+	 * @var decimal $id
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 * 
+	 */
+	private $id;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	/*	-----------------------------------------------------------------------------------------	*/
+	
+	public function __construct() {
+		$this->categories = new ArrayCollection();
+	}
+	
+	}
+	
+	
